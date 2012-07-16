3279e054d0e170d762f075226cf92977f7c8f676
Xu Zhao <nuklly@gmail.com>
2012-07-16 14:58:56 +0800

    thumips-qemu

---
+++ hw/mips-bios.h
@@ -0,0 +1,9 @@
+#include "cpu.h"
+
+/* size of mips bios is 4M? */
+#define BIOS_SIZE (4 * 1024 * 1024)
+#ifdef TARGET_WORDS_BIGENDIAN
+#define BIOS_FILENAME "mips_bios.bin"
+#else
+#define BIOS_FILENAME "mipsel_bios.bin"
+#endif
